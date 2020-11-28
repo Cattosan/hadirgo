9ca36c38c760547af88f06f8e5e5eb75759a8728
Akira Rafhael Janson Pattirane <akira.rafhael@ti.ukdw.ac.id>
2020-11-29 00:28:55 +0700

    Perbaikan fitur ubah password

--- HADIRGo/src/main/java/com/mycompany/hadirgo/UbahPasswordController.java
+++ HADIRGo/src/main/java/com/mycompany/hadirgo/UbahPasswordController.java
@@ -5,23 +5,57 @@
  */
 package com.mycompany.hadirgo;
 
-import java.net.URL;
-import java.util.ResourceBundle;
-import javafx.fxml.Initializable;
+import java.io.IOException;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.PasswordField;
 
-/**
- * FXML Controller class
- *
- * @author KUCI
- */
-public class UbahPasswordController implements Initializable {
 
-    /**
-     * Initializes the controller class.
-     */
-    @Override
-    public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
+public class UbahPasswordController{
+    private String pwdLama;
+    private String pwdBaru;
+    private String confirmPwd;
     
+    @FXML
+    private PasswordField passwordLama;
+    @FXML
+    private PasswordField passwordBaru;
+    @FXML
+    private PasswordField confirmPasswordBaru;
+    
+    @FXML
+    private void cekPassword(ActionEvent event) throws IOException{
+        pwdLama = passwordLama.getText().trim();
+        pwdBaru = passwordBaru.getText().trim();
+        confirmPwd = confirmPasswordBaru.getText().trim();
+        
+        if(!pwdLama.equals(Home.pwd)){
+            notifikasiError("Password lama yang diinputkan salah");
+        }
+        else if(pwdLama.equals(pwdBaru)){
+            notifikasiError("Password lama tidak boleh sama dengan password baru");
+        } else if(!pwdBaru.equals(confirmPwd)){
+            notifikasiError("Konfirmasi password baru berbeda");
+        } else{
+            HadirGoDb.editPassword(Home.usr, pwdBaru);
+            notifikasiBerhasil("Password berhasil diubah");
+        }
+    }
+    
+    private void notifikasiError(String isiNotif){
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.setTitle("Error");
+        alert.setHeaderText(null);
+        alert.setContentText(isiNotif);
+        alert.showAndWait();
+    }
+    
+    private void notifikasiBerhasil(String isiNotif){
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle("Success");
+        alert.setHeaderText(null);
+        alert.setContentText(isiNotif);
+        alert.showAndWait();
+    }
 }
